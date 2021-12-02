d97159aac5e6c4ce2a164ff97de4f9f8cf4f0357
Chris Moore <git@chrismoore.dev>
2021-12-02 17:32:52 -0600

    initial commit - days 1 & 2

---
+++ day01/src/lib.rs
@@ -0,0 +1,88 @@
+use aoch::{AoCDay, DayResult};
+#[allow(unused_imports)] use aoch::DayError;
+#[cfg(test)] #[allow(unused_imports)]
+use aoch::{DayPart, run_test, test_runner, daystr};
+
+pub struct Day01 {
+	nums: Vec<isize>,
+}
+impl Day01 {
+}
+
+impl AoCDay for Day01 {
+	type Answer = usize;
+
+	fn day() -> u8 { 01 }
+	fn name() -> &'static str { "Sonar Sweep" }
+
+	fn parse(input: &str) -> DayResult<Self> {
+		aoch::parsing::from_lines(input)	
+			.map(|nums| Day01 { nums })
+			.map_err(|e| e.into())
+	}
+	fn part1(&mut self) -> DayResult<Self::Answer> {
+		let mut increased = 0;
+		for i in 1..self.nums.len() {
+			if self.nums[i-1] < self.nums[i] {
+				increased += 1;
+			}
+		}
+		Ok(increased)
+	}
+	fn part2(&mut self) -> DayResult<Self::Answer> {
+		let mut increased = 0;
+
+		let windowed: Vec<isize> = self.nums.windows(3)
+			.map(|s| s.iter().sum())
+			.collect();
+
+		for i in 1..windowed.len() {
+			if windowed[i-1] < windowed[i] {
+				increased += 1;
+			}
+		}
+
+		Ok(increased)
+	}
+}
+
+/*
+#[test]
+fn fuel_calc() {
+	let cases = [
+		(100756, 33583),
+	];
+	run_test(|n| DayMe::calc_fuel(*n), &cases);
+}
+*/
+
+#[cfg(test)]
+const TEST_INPUT: &'static str = "
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+";
+
+#[test]
+fn part1() {
+	let cases = [
+		(TEST_INPUT, 7),
+		(daystr!("01"), 1502),
+	];
+	test_runner::<Day01, _>(DayPart::Part1, &cases);
+}
+#[test]
+fn part2() {
+	let cases = [
+		(TEST_INPUT, 5),
+		(daystr!("01"), 1538),
+	];
+	test_runner::<Day01, _>(DayPart::Part2, &cases);
+}
---
+++ day02/src/lib.rs
@@ -0,0 +1,150 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+use thiserror::Error;
+
+use aoch::{AoCDay, DayResult};
+#[allow(unused_imports)] use aoch::DayError;
+#[cfg(test)] #[allow(unused_imports)]
+use aoch::{DayPart, run_test, test_runner, daystr};
+
+pub struct Day02 {
+	cmds: Vec<Command>,
+}
+impl Day02 {
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+struct Position {
+	horiz: isize,
+	depth: isize,
+	aim: isize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Command(Direction, isize);
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+	Forward,
+	Down,
+	Up,
+}
+
+
+#[derive(Error, Debug)]
+#[error("Invalid direction provided: {0}")]
+struct BadDirection(String);
+
+#[derive(Error, Debug)]
+#[error("bad command")]
+enum BadCommand {
+	BadElementCount,
+	BadDirection(#[from] BadDirection),
+	BadNumber(#[from] ParseIntError),
+}
+
+
+impl FromStr for Command {
+    type Err = BadCommand;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.split(' ').collect::<Vec<_>>().as_slice() {
+			&[dir, num] => {
+				Ok(Command(
+					dir.parse()?,
+					num.parse()?,
+				))
+			},
+			_ => Err(BadCommand::BadElementCount),
+		}
+    }
+}
+impl FromStr for Direction {
+	type Err = BadDirection;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+			"forward" => Ok(Direction::Forward),
+			"down" => Ok(Direction::Down),
+			"up" => Ok(Direction::Up),
+			_ => Err(BadDirection(s.to_owned()))
+		}
+    }
+}
+
+impl AoCDay for Day02 {
+	type Answer = isize;
+
+	fn day() -> u8 { 02 }
+	fn name() -> &'static str { "Dive!" }
+
+	fn parse(input: &str) -> DayResult<Self> {
+		aoch::parsing::from_lines(input)	
+			.map(|cmds| Day02 { cmds })
+			.map_err(|e| DayError::Wrapped(Box::new(e)))
+	}
+	fn part1(&mut self) -> DayResult<Self::Answer> {
+		
+		let mut pos = Position::default();
+		for cmd in &self.cmds {
+			match cmd.0 {
+				Direction::Forward => pos.horiz += cmd.1,
+				Direction::Down => pos.depth += cmd.1,
+				Direction::Up => pos.depth -= cmd.1,
+			}
+		}
+
+		Ok(pos.horiz * pos.depth)
+	}
+	fn part2(&mut self) -> DayResult<Self::Answer> {
+		let mut pos = Position::default();
+		for cmd in &self.cmds {
+			match cmd.0 {
+				Direction::Forward => {
+					pos.horiz += cmd.1;
+					pos.depth += pos.aim * cmd.1;
+				},
+				Direction::Down => pos.aim += cmd.1,
+				Direction::Up => pos.aim -= cmd.1,
+			}
+		}
+
+		Ok(pos.horiz * pos.depth)
+	}
+}
+
+/*
+#[test]
+fn fuel_calc() {
+	let cases = [
+		(100756, 33583),
+	];
+	run_test(|n| DayMe::calc_fuel(*n), &cases);
+}
+*/
+
+#[cfg(test)]
+const TEST_INPUT: &'static str = "
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+";
+
+#[test]
+fn part1() {
+	let cases = [
+		(TEST_INPUT, 150),
+		(daystr!("02"), 1451208),
+	];
+	test_runner::<Day02, _>(DayPart::Part1, &cases);
+}
+#[test]
+fn part2() {
+	let cases = [
+		(TEST_INPUT, 900),
+		(daystr!("02"), 1620141160),
+	];
+	test_runner::<Day02, _>(DayPart::Part2, &cases);
+}
